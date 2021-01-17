02359121c40422de9917e63b25259c78c06563cb
arjunsingh <arjunksingh1027@gmail.com>
2021-01-17 00:31:07 -0700

    app change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Container from "./components/Container";
-import Employee from "./components/Employee";
-import Search from "./components/Search";
-import API from "./utils/API";
 import "./App.css";
+import Container from "./components/Container";
 
 
+function App (){
+  return (
+    <Container>
 
+    </Container>
+  );
+}
 
 export default App;
